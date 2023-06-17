2581628adab4ff55cddb7dfbb17416f0381c6ac6
maxkleshchyonok <56303675+maxkleshchyonok@users.noreply.github.com>
2023-06-17 03:21:41 +0300

    feat: add solutions to args

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+    // Write your code here
+   let str = '';
+   Object.keys(process.env).filter(el => el.includes('RSS_')).forEach(key => {
+      str += `${key}=${process.env[`${key}`]}; `;
+   });
+   console.log(str.slice(0, -2));
 };
 
 parseEnv();

5420ea9d55d27ffeb0b77cac39f67e064c199263
maxkleshchyonok <56303675+maxkleshchyonok@users.noreply.github.com>
2023-06-20 00:58:30 +0300

    feat: add solutions to cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,21 @@
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const pathToFile = path.join(__dirname, 'files', 'script.js');
+import { spawn } from 'child_process';
+const { stdin, stdout } = process;
+
 const spawnChildProcess = async (args) => {
     // Write your code here
+    const process = spawn('node', [pathToFile, ...args]);
+    stdin.on('data', (data) => {
+        process.stdin.write(data);
+    });
+    process.stdout.on('data', (data) => {
+        stdout.write(data);
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ['someArgument1', 'someArgument2', 'someArgument3'] );

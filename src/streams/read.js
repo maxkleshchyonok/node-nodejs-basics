fe42ffc11dafa6380e818b5404ae5c138a917ded
maxkleshchyonok <56303675+maxkleshchyonok@users.noreply.github.com>
2023-06-19 03:01:19 +0300

    feat: add solutions to streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
-const read = async () => {
-    // Write your code here 
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const pathToFile = path.join(__dirname, 'files', 'fileToRead.txt');
+const { stdout } = process;
+
+const readable = fs.createReadStream(pathToFile, {encoding: 'utf-8'});
+
+const read = async (readable) => {
+    // Write your code here
+    for await (const chunk of readable) {
+        stdout.write(chunk);
+    }
 };
 
-await read();
+await read(readable);
